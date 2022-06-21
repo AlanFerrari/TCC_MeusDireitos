d752491e9ddedbef1a16a30a813438e536006169
Alan Ferrari Silva <alanferrari33@gmail.com>
2022-06-21 16:36:59 -0300

    nova parte baseada no hero

--- app/src/main/java/br/com/etecia/meus_direitos/CadastroAdvogado.java
+++ app/src/main/java/br/com/etecia/meus_direitos/CadastroAdvogado.java
@@ -7,30 +7,46 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-import br.com.etecia.meus_direitos.objetos.User;
+import br.com.etecia.meus_direitos.objetos.API_IBGE;
+import br.com.etecia.meus_direitos.objetos.Advogados;
+import br.com.etecia.meus_direitos.objetos.Cidades;
+import br.com.etecia.meus_direitos.objetos.Estados;
+import br.com.etecia.meus_direitos.objetos.Subdistritos;
 
 public class CadastroAdvogado extends AppCompatActivity {
 
+    private static final int CODE_GET_REQUEST = 1024;
+    private static final int CODE_POST_REQUEST = 1025;
+
     Button btnCadastrarAdvogado;
     ImageView voltar;
-    EditText edtNomeAdvogado;
-    EditText edtEmail;
-    EditText edtTelefone;
-    EditText edtCidade;
-    EditText edtEstado;
-    EditText edtRegistroOAB;
-    EditText edtSenha;
+    EditText edtNomeAdvogado, edtEmail, edtTelefone, edtRegistroOAB, edtSenha;
+    Spinner spinnerEstados, spinnerCidades, spinnerSubdistritos;
+
+    Cidades[] municipios = null;
+    boolean isUpdating = false;
+    List<Advogados> AdvogadosList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +58,14 @@ public class CadastroAdvogado extends AppCompatActivity {
         edtNomeAdvogado = findViewById(R.id.nomeAdvogado);
         edtEmail = findViewById(R.id.email);
         edtTelefone = findViewById(R.id.telefone);
-        edtCidade = findViewById(R.id.cidade);
-        edtEstado = findViewById(R.id.estado);
+        spinnerCidades = findViewById(R.id.cidade);
+        spinnerEstados = findViewById(R.id.estado);
         edtRegistroOAB = findViewById(R.id.registroOAB);
         edtSenha = findViewById(R.id.senha);
+
+        spinnerEstados = findViewById(R.id.filtrarEstado);
+        spinnerCidades = findViewById(R.id.filtrarCidade);
+        spinnerCidades.setEnabled(false);
 
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,19 +79,141 @@ public class CadastroAdvogado extends AppCompatActivity {
         findViewById(R.id.btncadastrarAdvogado).setOnClickListener(view -> {
             //se o usuário pressionou o botão registrar
             //aqui vamos registrar o usuário no servidor
-            registerUser();
             Intent intent = new Intent(getApplicationContext(), Chip_filtro_areas.class);
+            registerUser();
             startActivity(intent);
             finish();
         });
+
+        String respostaEstados = executaApiIBGE("estado");
+
+        Gson gsonEstados = new GsonBuilder().setPrettyPrinting().create();
+        final Estados[] estados = gsonEstados.fromJson(String.valueOf(respostaEstados), Estados[].class);
+
+        final ArrayList<String> estadosParaSpinner = new ArrayList<>();
+
+        for (Estados estado: estados){
+            estadosParaSpinner.add(estado.getNome());
+        }
+
+        //Ordena em ordem alfabética
+        Collections.sort(estadosParaSpinner);
+
+        estadosParaSpinner.add(0, "Selecione o Estado");
+
+        //Criando um array de estados no spinner
+        ArrayAdapter<String> adapterEstados = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, estadosParaSpinner);
+        spinnerEstados.setAdapter(adapterEstados);
+
+        spinnerEstados.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                for (Estados estado : estados) {
+                    if (estado.getNome().equals(spinnerEstados.getSelectedItem().toString())) {
+                        solicitarMunicipios(estado.getSigla());
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerCidades.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                for (Cidades cidade : municipios) {
+                    if (cidade.getNome().equals(spinnerCidades.getSelectedItem().toString())) {
+                        solicitarSubdistritos(String.valueOf(cidade.getId()));
+                    }
+                }
+            }
+
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void solicitarMunicipios(String siglaEstado) {
+        String respostaMunicipios = executaApiIBGE("municipio", siglaEstado);
+
+        Gson gsonMunicipios = new GsonBuilder().setPrettyPrinting().create();
+        municipios = gsonMunicipios.fromJson(String.valueOf(respostaMunicipios), Cidades[].class);
+
+        final ArrayList<String> municipiosParaSpinner = new ArrayList<>();
+        final ArrayList<String> idMunicipios = new ArrayList<>();
+
+        for (Cidades cidade: municipios){
+            municipiosParaSpinner.add(cidade.getNome());
+            idMunicipios.add(String.valueOf(cidade.getId()));
+        }
+
+        //Ordena em ordem alfabética
+        Collections.sort(municipiosParaSpinner);
+
+        municipiosParaSpinner.add(0, "Selecione a Cidade");
+
+        spinnerCidades.setEnabled(true);
+
+        //Criando um array de municipios no spinner
+        ArrayAdapter<String> adapterMunicipios = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, municipiosParaSpinner);
+        spinnerCidades.setAdapter(adapterMunicipios);
+
+    }
+
+    private void solicitarSubdistritos(String idMunicipio) {
+        String respostaSubdistritos = executaApiIBGE("subdistrito", idMunicipio);
+
+        Gson gsonSubdistritos = new GsonBuilder().setPrettyPrinting().create();
+        Subdistritos[] subdistritos = gsonSubdistritos.fromJson(String.valueOf(respostaSubdistritos), Subdistritos[].class);
+
+        final ArrayList<String> subdistritosParaSpinner = new ArrayList<>();
+
+        for (Subdistritos subdistrito: subdistritos){
+            subdistritosParaSpinner.add(subdistrito.getNome());
+        }
+
+        //Ordena em ordem alfabética
+        Collections.sort(subdistritosParaSpinner);
+
+        subdistritosParaSpinner.add(0, "Selecione o Subdistrito");
+
+        spinnerSubdistritos.setEnabled(true);
+
+        //Criando um array de municipios no spinner
+        ArrayAdapter<String> adapterSubdistritos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, subdistritosParaSpinner);
+        spinnerSubdistritos.setAdapter(adapterSubdistritos);
+
+    }
+
+    private String executaApiIBGE (String... params) {
+        API_IBGE api_ibge = new API_IBGE();
+
+        String respostaIBGE = null;
+
+        try {
+            respostaIBGE = api_ibge.execute(params).get();
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return respostaIBGE;
     }
 
     private void registerUser() {
         final String usuario = edtNomeAdvogado.getText().toString().trim();
         final String email = edtEmail.getText().toString().trim();
         final String senha = edtSenha.getText().toString().trim();
-        final String cidade = edtCidade.getText().toString().trim();
-        final String estado = edtEstado.getText().toString().trim();
+        final String estado = spinnerEstados.getSelectedItem().toString();
+        final String cidade = spinnerCidades.getSelectedItem().toString();
         final String numeroOAB = edtRegistroOAB.getText().toString().trim();
         final String telefone = edtTelefone.getText().toString().trim();
 
@@ -102,17 +244,17 @@ public class CadastroAdvogado extends AppCompatActivity {
             return;
         }
 
-        if (TextUtils.isEmpty(cidade)) {
-            edtCidade.setError("Digite a cidade onde mora");
-            edtCidade.requestFocus();
+      /*  if (TextUtils.isEmpty(cidade)) {
+            spinnerCidades.setError("Selecione a cidade onde mora");
+            spinnerCidades.requestFocus();
             return;
         }
 
         if (TextUtils.isEmpty(estado)) {
-            edtEstado.setError("Insira um estado onde mora");
-            edtEstado.requestFocus();
+            spinnerEstados.setError("Selecione um estado onde mora");
+            spinnerEstados.requestFocus();
             return;
-        }
+        }*/
 
         if (TextUtils.isEmpty(numeroOAB)) {
             edtRegistroOAB.setError("Insira seu registro na OAB");
@@ -128,14 +270,14 @@ public class CadastroAdvogado extends AppCompatActivity {
 
         //se passar em todas as validações
 
-        class RegisterUser extends AsyncTask<Void, Void, String> {
+      /*  class RegisterUser extends AsyncTask<Void, Void, String> {
 
             private ProgressBar progressBar;
 
             @Override
             protected String doInBackground(Void... voids) {
                 //criando objeto manipulador de requisição
-                RequestHandler requestHandler = new RequestHandler();
+                RequestHandler requestHandler = new RequestHandler();*/
 
                 //criando parâmetros de requisição
                 HashMap<String, String> params = new HashMap<>();
@@ -147,11 +289,86 @@ public class CadastroAdvogado extends AppCompatActivity {
                 params.put("numeroOAB", numeroOAB);
                 params.put("telefone", telefone);
 
-                //retornando a resposta
-                return requestHandler.sendPostRequest(URLs.URL_REGISTER, params);
-            }
+                //Essa parte é nova, baseada no projeto hero
+        PerformNetworkRequest request = new PerformNetworkRequest(URLs.URL_REGISTER, params, CODE_POST_REQUEST);
+        request.execute();
 
-            @Override
+        btnCadastrarAdvogado.setText("Adicionar");
+
+        edtNomeAdvogado.setText("");
+        edtEmail.setText("");
+        edtTelefone.setText("");
+        edtRegistroOAB.setText("");
+        edtSenha.setText("");
+        spinnerEstados.setSelection(0);
+        spinnerCidades.setSelection(0);
+        isUpdating = false;
+
+
+        //retornando a resposta
+        //return requestHandler.sendPostRequest(URLs.URL_REGISTER, params);
+    }
+
+    private void refreshUserList(JSONArray usuario) throws JSONException {
+        AdvogadosList.clear();
+
+        for (int i = 0; i < usuario.length(); i++) {
+            JSONObject obj = usuario.getJSONObject(i);
+
+            AdvogadosList.add(new Advogados(
+                    obj.getInt("imagem"),
+                    obj.getString("username"),
+                    obj.getString("cidade"),
+                    obj.getString("estado"),
+                    obj.getString("area_atuacao")
+            ));
+        }
+    }
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    Toast.makeText(getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                    refreshUserList(object.getJSONArray("usuario"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler requestHandler = new RequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
+    }
+           /* @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
 
@@ -194,5 +411,5 @@ public class CadastroAdvogado extends AppCompatActivity {
         //executando a tarefa assíncrona
         RegisterUser ru = new RegisterUser();
         ru.execute();
-    }
+    }*/
 }
